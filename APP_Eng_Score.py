b7655271a895cb53c03d52ddfa9208f818152344
Ani Milina <118534615+AniMilina@users.noreply.github.com>
2023-05-05 10:31:04 +0300

    Update APP_Eng_Score.py

--- APP_Eng_Score.py
+++ APP_Eng_Score.py
@@ -2,10 +2,10 @@
 # coding: utf-8
 
 # Install necessary packages
-!pip install --upgrade pip
-!pip install -r requirements.txt
-!pip uninstall click
-!pip install click==7.1.2 
+pip install --upgrade pip
+pip install -r requirements.txt
+pip uninstall click
+pip install click==7.1.2 
 
 import pandas as pd
 import joblib
